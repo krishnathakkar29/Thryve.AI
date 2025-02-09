a865e16e7510ed2bc4f2c1857c1299b46c9f830d
Krishna Thakkar <work.krishnat@gmail.com>
2025-02-09 10:29:36 +0530

    .

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -12,22 +12,21 @@ function Landing() {
           className="p-8 mb-24 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full bg-[#fff] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
         >
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-blue-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black group-hover:to-black">
-            Start For Free Today
+            Helping employees succeed
           </span>
         </Button>
         <h1 className="max-w-5xl text-center text-5xl md:text-8xl bg-gradient-to-r from-blue-900 via-blue-500 to-blue-300 bg-clip-text text-transparent font-sans font-bold">
-          Automate Your Work With Fuzzie
+          ThryVe (Thrive + AI)
         </h1>
         <div className=" w-full h-24 flex justify-center items-center absolute bottom-20">
           <div className="text-3xl font-semibold lg:text-2xl text-black">
             <Typewriter
               options={{
                 strings: [
-                  "AI-Powered Crop Management",
-                  "Real-time Weather Monitoring",
-                  "Smart Irrigation Systems",
-                  "Precision Agriculture Tools",
-                  "Sustainable Farming Solutions",
+                  "AI HR Manager",
+                  "Schedule Meetings",
+                  "Smart Assistant",
+                  "Automate and Suceed",
                 ],
                 autoStart: true,
                 loop: true,
@@ -40,9 +39,9 @@ function Landing() {
         </div>
       </section>
 
-      <div className="w-full h-screen bg-[#EDE7DB]">
+      {/* <div className="w-full h-screen bg-[#EDE7DB]">
         <Model />
-      </div>
+      </div> */}
     </div>
   );
 }
